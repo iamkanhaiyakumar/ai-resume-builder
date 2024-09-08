57d6cb4fb56bfc0eeab5d6c16cda01ce475c9664
Kanhaiya Kumar <kanhaiyak0104@gmail.com.com>
2024-09-08 15:35:33 +0530

    feat: Add dependencies for Clerk authentication
    - Added "@clerk/clerk-react" as a dependency in package.json
    - Added "type": "module" in .vite/deps_temp_a0ab1e6f/package.json
    - Created postcss.config.js with tailwindcss and autoprefixer plugins
    - Added react.js.map file in .vite/deps_temp_a0ab1e6f directory
    - Created utils.js file in my-project/src/lib directory
    - Added react.js file in .vite/deps_temp_a0ab1e6f directory
    - Created dashboard/index.jsx file in my-project/src/dashboard directory
    - Created jsconfig.json file in my-project directory
    - Created vite.config.js file in my-project directory
    - Created .gitignore file in my-project directory
    - Created sign-in/index.jsx file in my-project/src/auth directory
    - Created home/index.jsx file in my-project/src/home directory
    - Created README.md file in my-project directory
    - Created index.html file in my-project directory
    - Created components.json file in my-project directory
    - Created App.jsx file in my-project/src directory
    - Created App.css file in my-project/src directory
    - Created Header.jsx file in my-project/src/components/custom directory
    - Created main.jsx file in my-project/src directory
    - Created eslint.config.js file in my-project directory
    - Added dependencies in package.json

---
+++ my-project/src/home/index.jsx
@@ -0,0 +1,15 @@
+import Header from '@/components/custom/Header'
+import { UserButton } from '@clerk/clerk-react'
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='my-1'>
+    <Header/>
+    Landing Screen
+    {/* <UserButton/> */}
+    </div>
+  )
+}
+
+export default Home
